39b2be439cfa1f825488c23ff926fccb65cca7cc
Aadhesharjun Senthilkumar <aadheshvs@gmail.com>
2024-04-07 14:08:38 -0500

    vRTC test

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -131,7 +131,7 @@ int32_t rtc_close (int32_t fd){ //Aadhesh
 int32_t vrtc_process (int32_t fd, const void * buf, int32_t nbytes){ //Aadhesh
     if(nbytes != 4) return -1; //Return failure if more or less than 4 bytes passed in
     rtc_set_frequency(1024); //Set to max frequency
-    while(vrtc_int < (int32_t *) buf){ // x virtual interrupts for 1 1024-Hz interrupt
+    while(vrtc_int < (int32_t *) buf){ // x virtual interrupts for 1 1024-Hz interrupt (x = buf)
         rtc_read(fd,buf,nbytes);
         vrtc_int++;
     }
